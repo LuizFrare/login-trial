314a64a8c4d869a40fb34b358879b8612a9b2467
LuizFrare <luizeduardo.frare@outlook.com>
2023-12-12 20:20:15 -0300

    feat: create password icons

---
+++ src/assets/icons/OpenEyeIcon.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const OpenEyeIcon = ({ fill, width, height }) => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width={width} height={height} viewBox="0 0 18 15" fill="none">
+      <path
+        d="M11.5 7.49984C11.5 8.88055 10.3808 9.99984 9.00004 9.99984C7.61933 9.99984 6.50004 8.88055 6.50004 7.49984C6.50004 6.11913 7.61933 4.99984 9.00004 4.99984C10.3808 4.99984 11.5 6.11913 11.5 7.49984Z"
+        stroke={fill}
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M1.04858 7.49981C2.11048 4.1189 5.26905 1.6665 9.00041 1.6665C12.7318 1.6665 15.8904 4.11893 16.9522 7.49987C15.8904 10.8808 12.7318 13.3332 9.00042 13.3332C5.26905 13.3332 2.11046 10.8807 1.04858 7.49981Z"
+        stroke={fill}
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+};
+
+export default OpenEyeIcon;
