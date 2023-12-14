ba61ffdfb7f5cb0ac25dc860b98e40626d97d5ab
LuizFrare <luizeduardo.frare@outlook.com>
2023-12-13 21:17:50 -0300

    refactor: remove button problem

--- src/components/molecules/LoginOptions/LoginOptions.jsx
+++ src/components/molecules/LoginOptions/LoginOptions.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import "./styles.css";
-import { Button, Checkbox } from "../../atoms";
+import { Checkbox } from "../../atoms";
 import { Link } from "react-router-dom";
 
 const LoginOptions = () => {
   return (
     <div className="login-options">
       <Checkbox label="Remember me" />
-      <Link to="/forgot-password">
-        <Button type="button" variant="secondary">Forgot my password</Button>
+      <Link to="/forgot-password" className="secondary">
+      Forgot my password
       </Link>
     </div>
   );
