7f89d503595b41a375915830b8f9cec81d3016a9
Eduardo Rodrigues Amaral <33547699+SrAmaral@users.noreply.github.com>
2024-10-30 11:30:12 -0300

    1

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,6 @@ function App() {
   console.log('hack tober fest')
   console.log('hack tober fest is')
   console.log('hack tober fest is nice')
-  console.log('hack tober fest is nice!!!!!')
 
   return (
     <BrowserRouter>
