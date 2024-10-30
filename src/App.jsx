70e10314299660f629e55b0214cfa160ede55c87
Eduardo Rodrigues Amaral <33547699+SrAmaral@users.noreply.github.com>
2024-10-30 11:27:33 -0300

    tober

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { HomePage, ForgotPassword } from "./components/organisms";
 
 function App() {
   console.log('hack')
+  console.log('hack tober')
   return (
     <BrowserRouter>
       <Routes>
