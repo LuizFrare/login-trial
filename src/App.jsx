17fbc2326ea8d3963edbd69de616b509fbe26594
Eduardo Rodrigues Amaral <33547699+SrAmaral@users.noreply.github.com>
2024-10-30 11:26:50 -0300

    hack

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { HomePage, ForgotPassword } from "./components/organisms";
 
 function App() {
+  console.log('hack')
   return (
     <BrowserRouter>
       <Routes>
